fc75cf4dea57be52da15387459b79d547a12ba72
KorKux <cristhianeduardo03@hotmail.com>
2020-10-04 20:14:02 -0500

    feat: add measurements by date and EEUU Map Pollution

--- hazards_detection/dashboard/utils/utils.py
+++ hazards_detection/dashboard/utils/utils.py
@@ -16,6 +16,8 @@ labels_dict = {
 def get_dataset():
     dataset = pd.read_csv(
         'https://github.com/KorKux1/Automated-Detection-of-Hazards/raw/develop/Datasets/Air%20Quality%20Data/PM2.5_dataset.csv')
+
+    dataset['stime'] = pd.to_datetime(dataset['stime'])
     dataset = dataset.drop(['Unnamed: 0'], axis=1)
     dummy = [1 if i >= 2.5 else 0 for i in dataset['goes_measurement']]
     dataset['PM High'] = dummy
